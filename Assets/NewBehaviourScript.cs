51dc0c27b2d94ae35f73f707aea96e2d7c19781a
renya <sk8.dance.eureka@gmail.com>
2017-01-25 22:34:10 +0900

    adsdadsda

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	float n =0;
+	float bounse = 10;
+	void Update () {
+
+		n += Time.deltaTime;
+		bounse -= Time.deltaTime;
+
+		if (bounse > 0.01f) {
+			this.transform.position = new Vector3 (n, Mathf.Abs (bounse * Mathf.Sin (Time.time)), 0);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# You can use bound by not using rigidboy
+
+
+
+
+Sometimes I dont want to use rigidbody, so I made this script.
